dfb220ed129a738343416ee62c7ed51bffc9d492
Weiwei Yang <2308518201@qq.com>
2023-09-10 14:26:40 +0800

    deploy_port

--- server.py
+++ server.py
@@ -1,18 +1,33 @@
 from flask import Flask, send_file, Response, send_from_directory
 from flask import jsonify,request,render_template
 app = Flask(__name__)
+deploy_port = "http://114.55.178.110/" #http://127.0.0.1:5000  #"http://114.55.178.110/"
 
-@app.route("/",methods = ["GET","POST"])
-def mainpage():
-    return render_template("./mainContent/index.html")
 
 @app.route("/cover",methods = ["GET","POST"])
 def cover():
-    return render_template("./cover/index.html")
+    global deploy_port
+    kwargs = {
+        "deploy_port": deploy_port
+    }    
+    return render_template("./cover/index.html",**kwargs)
+
+
+@app.route("/",methods = ["GET","POST"])
+def mainpage():
+    global deploy_port
+    kwargs = {
+        "deploy_port": deploy_port
+    }
+    return render_template("./mainContent/index.html",**kwargs)
 
 @app.route("/<blogindex>",methods = ["GET","POST"])
 def blogs(blogindex):
-    return render_template("./blogs/{}/index.html".format(blogindex))
+    global deploy_port
+    kwargs = {
+        "deploy_port": deploy_port
+    }
+    return render_template("./blogs/{}/index.html".format(blogindex),**kwargs)
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1",port=5000 ,debug=True)  # 172.20.10.7
