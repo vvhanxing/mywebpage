5471760e7be7afba04709fba095a815b862afabf
Weiwei Yang <2308518201@qq.com>
2023-09-10 20:58:48 +0800

    deploy_vr

--- server.py
+++ server.py
@@ -29,6 +29,14 @@ def blogs(blogindex):
     }
     return render_template("./blogs/{}/index.html".format(blogindex),**kwargs)
 
+@app.route("/ar",methods = ["GET","POST"])
+def ar():
+    global deploy_port
+    kwargs = {
+        "deploy_port": deploy_port
+    }
+    return render_template("./love_2.html")
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1",port=5000 ,debug=True)  # 172.20.10.7
     #app.run(host="172.20.10.7",port=5000 ,debug=True) 
