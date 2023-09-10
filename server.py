3446acb5036e9ee76f0149e722ea80a4dab6d6f7
Weiwei Yang <2308518201@qq.com>
2023-09-10 08:11:10 +0800

    add pages

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, send_file, Response, send_from_directory
+from flask import jsonify,request,render_template
+app = Flask(__name__)
+
+@app.route("/",methods = ["GET","POST"])
+def mainpage():
+    return render_template("./mainContent/index.html")
+
+@app.route("/cover",methods = ["GET","POST"])
+def cover():
+    return render_template("./cover/index.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1",port=5000 ,debug=True)  # 172.20.10.7
+    #app.run(host="172.20.10.7",port=5000 ,debug=True) 
+
+
+
